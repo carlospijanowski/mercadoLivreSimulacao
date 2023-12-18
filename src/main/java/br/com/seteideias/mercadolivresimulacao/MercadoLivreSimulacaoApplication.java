561860fdd09c9e48793f5711deef2df7561e559b
Carlos Pijanowski Cartaxo <carlospc1978@gmail.com>
2023-12-17 21:02:34 -0300

    Initial commit

---
+++ src/main/java/br/com/seteideias/mercadolivresimulacao/MercadoLivreSimulacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.seteideias.mercadolivresimulacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoLivreSimulacaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MercadoLivreSimulacaoApplication.class, args);
+    }
+
+}
